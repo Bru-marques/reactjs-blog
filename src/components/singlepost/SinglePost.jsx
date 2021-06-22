bef25d53a5692b85f4b4c4f57722df3ab2b95813
Bruna Marques <brmarques1108@gmail.com>
2021-06-22 10:23:37 +0100

    single-post done

--- src/components/singlepost/SinglePost.jsx
+++ src/components/singlepost/SinglePost.jsx
@@ -1,5 +1,58 @@
 import "./singlePost.css";
 
 export default function SinglePost() {
-  return <div className="single-post">Single Post</div>;
+  return (
+    <div className="single-post">
+      <div className="single-post-wrapper">
+        <img
+          className="single-post-img"
+          src="https://images.unsplash.com/photo-1519643381401-22c77e60520e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1053&q=80"
+          alt="single-post-img"
+        />
+        <h1 className="single-post-title">
+          Loren ipsum dolor.
+          <div className="single-post-edit">
+            <i className="single-post-icon far fa-edit"></i>
+            <i className="single-post-icon far fa-trash-alt"></i>
+          </div>
+        </h1>
+        <div className="single-post-info">
+          <span>
+            Author: <b className="single-post-author">Bruna</b>
+          </span>
+          <span>1 hour ago</span>
+        </div>
+        <p className="single-post-desc">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
+          quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
+          Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
+          eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
+          error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
+          impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
+          odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos!
+          <br />
+          <br />
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
+          quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
+          Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
+          eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
+          error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
+          impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
+          odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
+          iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
+          a odit modi eos! Lorem, ipsum dolor sit amet consectetur.
+        </p>
+      </div>
+    </div>
+  );
 }
